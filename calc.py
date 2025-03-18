25b2385e197c2c8f453249f76987ebbffbc328b2
Jan Čmuchař <126028643+FlagM8@users.noreply.github.com>
2025-03-18 11:10:48 +0100

    test of test

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 class Calculator:
     def add(self, a, b):
-        return a + b
+        return a + b -1
     def subtract(self, a, b):
-        return a - b
+        return a - b -1 
     def multiply(self, a, b):
-        return a * b
+        return a * b -1 
     def divide(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero")
-        return a / b
+        return a / b -1
